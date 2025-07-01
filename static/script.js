f896c82584e38dd59b9e179caec67d938b8daa7b
SebastianAlcantarr <sebastianalcantar323@gmail.com>
2025-06-30 17:06:13 -0700

    nombre

--- static/script.js
+++ static/script.js
@@ -20,44 +20,45 @@ const div = document.getElementById('mensaje-popup');
   }, 1500);
 }
 
+function guardar_bd() {
+  document.addEventListener('DOMContentLoaded', function (message) {
+    document.getElementById('bookAppointment').addEventListener('click', async function (message) {
+      const serviceType = document.getElementById('serviceType').value;
+      const date = document.getElementById('appointmentDate').value;
+      const time = document.getElementById('appointmentTime').value;
+      const nombre = document.getElementById('appointmentname').value;
 
-function reiniciarInputs() {
+      if (!serviceType || !date || !time || !nombre) {
+        llenarCampos('Por favor, completa todos los campos.');
+        return;
+      }
+      try {
+        const response = await fetch('/api/book', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            service_type: serviceType,
+            date: date,
+            time: time,
+            nombre: nombre,
+          })
+        });
+
+        if (response.ok) {
+          mostrarPopup('Cita agendada con éxito.');
+        } else {
+          llenarCampos('Llene todos los campos')
+        }
+      } catch (error) {
+        console.error('Error en la solicitud:', error);
+        alert('No se pudo conectar con el servidor.error', error);
+      }
+    });
+  });
+
   document.querySelectorAll('input[type="text"]').forEach(input => {
     input.value = '';
   });
+
 }
 
-    document.addEventListener('DOMContentLoaded', function (message) {
-      document.getElementById('bookAppointment').addEventListener('click', async function (message) {
-        const serviceType = document.getElementById('serviceType').value;
-        const date = document.getElementById('appointmentDate').value;
-        const time = document.getElementById('appointmentTime').value;
-        const nombre =document.getElementById('appointmentname').value;
-
-        if (!serviceType || !date || !time|| !nombre) {
-          llenarCampos('Por favor, completa todos los campos.');
-          return;
-        }
-        try {
-          const response = await fetch('/api/book', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                service_type: serviceType,
-                date: date,
-                time: time,
-                nombre:nombre,
-            })
-          });
-
-          if (response.ok) {
-            mostrarPopup('Cita agendada con éxito.');
-          } else {
-            llenarCampos('Llene todos los campos')
-          }
-        } catch (error) {
-          console.error('Error en la solicitud:', error);
-          alert('No se pudo conectar con el servidor.error', error);
-        }
-      });
-    });
